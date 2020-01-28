77592f02a72c7455308bb82d375698945d400c9f
Vincent Servio <vincentservio@gmail.com>
2020-01-28 20:59:02 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,20 @@
-
+class Artist 
+  attr_accessor :name, :songs
+  @@all = []  
+  def initialize(name)
+    @name = name
+  end
+   def self.all 
+     @@all
+   end
+   def self.destroy_all
+     all.clear
+   end
+   def save 
+     @@all << self
+   end 
+   def self.create(name)
+     
+    @@all << self.new(name).save
+   end 
+end
